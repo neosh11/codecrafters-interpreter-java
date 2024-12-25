bcc45e0066268c3ffbbf25fc00da3c2cb0cb96f3
Neosh <neosh11@gmail.com>
2024-12-25 23:17:55 +1100

    Create a basic Scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,22 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
-
+    if (args.length == 0) {
+      try {
+        runPrompt();
+      } catch (IOException e) {
+        System.err.println("Error reading input: " + e.getMessage());
+        System.exit(1);
+      }
+    }
     if (args.length < 2) {
       System.err.println("Usage: ./your_program.sh tokenize <filename>");
       System.exit(1);
@@ -28,12 +38,54 @@ public class Main {
       System.exit(1);
     }
 
-    // Uncomment this block to pass the first stage
-    // 
-    // if (fileContents.length() > 0) {
-    //   throw new RuntimeException("Scanner not implemented");
-    // } else {
-    //   System.out.println("EOF  null"); // Placeholder, remove this line when implementing the scanner
-    // }
+    if (fileContents.length() > 0) {
+      Scanner scanner = new Scanner(fileContents);
+      List<Token> tokens = scanner.scanTokens();
+      for (Token token : tokens) {
+        System.out.println(token);
+      }
+    } else {
+      System.out.println("EOF  null"); // Placeholder, remove this line when implementing the scanner
+    }
   }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null)
+        break;
+      run(line);
+    }
+  }
+
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+  }
+
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+    // For now, just print the tokens.
+    for (Token token : tokens) {
+      System.out.println(token);
+    }
+  }
+
+  static boolean hadError = false;
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+  private static void report(int line, String where,
+      String message) {
+    System.err.println(
+        "[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
+
 }
