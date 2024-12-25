6772d8d14f4ff8d99eb6ecf8c2141395d0209042
Neosh <neosh11@gmail.com>
2024-12-25 23:30:54 +1100

    Support single char operators

--- src/main/java/Scanner.java
+++ src/main/java/Scanner.java
@@ -84,6 +84,22 @@ class Scanner {
                 addToken(TokenType.RIGHT_BRACE);
                 break;
 
+            case ',':
+                addToken(TokenType.COMMA);
+                break;
+            case '-':
+                addToken(TokenType.MINUS);
+                break;
+            case '+':
+                addToken(TokenType.PLUS);
+                break;
+            case '*':
+                addToken(TokenType.STAR);
+                break;
+            case '.':
+                addToken(TokenType.DOT);
+                break;
+
             case '"':
                 string();
                 break;
--- src/main/java/TokenType.java
+++ src/main/java/TokenType.java
@@ -2,6 +2,7 @@ enum TokenType {
     // Single-character tokens.
     SEMICOLON, SLASH, LEFT_PAREN, RIGHT_PAREN,
     LEFT_BRACE, RIGHT_BRACE,
+    COMMA, MINUS, PLUS, STAR, DOT,
     // One or two character tokens.
     EQUAL, EQUAL_EQUAL,
     // Literals.
